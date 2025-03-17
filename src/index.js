f2331831751c738b853e4ed9171877bdafafec66
Zulvan Fahmi <zulvanfahmi1612@gmail.com>
2025-03-17 16:57:32 +0700

    initiation project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCiGMc23rRe4Jg7umsO9pG5sxE-e0-Jorw",
+    authDomain: "learnfirebase-76ba7.firebaseapp.com",
+    projectId: "learnfirebase-76ba7",
+    storageBucket: "learnfirebase-76ba7.firebasestorage.app",
+    messagingSenderId: "845952668841",
+    appId: "1:845952668841:web:87d2157f72ba73943985b8"
+};
+
+initializeApp(firebaseConfig)
