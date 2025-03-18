867add7133b657c85b74c87e2a298e61f49a6d3d
Zulvan Fahmi <zulvanfahmi1612@gmail.com>
2025-03-18 16:04:05 +0700

    feature delete and add book data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import { initializeApp } from 'firebase/app'
 import { 
     getFirestore,
     collection,
-    getDocs
+    getDocs,
+    addDoc,
+    deleteDoc,
+    doc
  } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -41,4 +44,33 @@ getDocs(colRef)
 })
 .catch(err => {
     console.log(err.message)
+})
+
+
+// Adding documents
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colRef, {
+        title: addBookForm.title.value,
+        author: addBookForm.author.value,
+    })
+    .then(() => {
+        addBookForm.reset()
+    })
+
+})
+
+// Deleting documents
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'books', deleteBookForm.id.value)
+    deleteDoc(docRef)
+    .then(() => {
+        deleteBookForm.reset()
+    })
+
 })
